9cb911a17dadd8bde861e6d938d51b1a3892e7da
Caden Wilcox <wilcox4357@gmail.com>
2024-03-29 23:33:02 -0700

    getting the frontend started in react native

---
+++ client/components/Register.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+
+const Register = (props) => {
+    return (
+        <View style={{flex:1}}>
+            <View style={{flex:1}} className='bg-slate-800'>
+            </View>
+            <View style={{flex:2}} className='bg-red-800'>
+            </View>
+        </View>
+    )
+}
+
+export default Register;
